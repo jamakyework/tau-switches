2f00e539e09215a4e6705140667ee2d86dfaf931
Millie Walsh <millie@primeacademy.io>
2016-11-28 16:53:43 -0600

    add switch and type coercion test

--- script.js
+++ script.js
@@ -5,9 +5,34 @@ var greetMe = function() {
   var dayOfWeek = document.getElementById('dayOfWeek').value;
   console.log('input value is = ', dayOfWeek);
 
+  switch (dayOfWeek) {
+    case 'Monday':
+      console.log(dayOfWeek + " is the worst day of the week");
+      break;
+    case 'Tuesday':
+      console.log("Taco " + dayOfWeek);
+      break;
+    case 'Wednesday':
+      console.log("Hump Day is "+ dayOfWeek);
+      break;
+    case 'Saturday':
+    // -- fall through --
+    case 'Sunday':
+      console.log('Happy Weekend!');
+      break;
+    default:
+      console.log("Happy " + dayOfWeek);
+  }
+};
 
-if('2' < 193804){
-  console.log('inside if');
-}else {
-  console.log('inside else');
-}
+var tripleEquals = function() {
+  var expressionNum = 3;
+
+  switch (expressionNum) {
+    case '3':
+      console.log('Matched string three');
+      break;
+    default:
+      console.log('Didn\'t match');
+    }
+};
