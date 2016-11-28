1f92d757d5cb277f0ed97c519aeb559c875e466a
Millie Walsh <millie@primeacademy.io>
2016-11-28 15:00:28 -0600

    oops

--- script.js
+++ script.js
@@ -5,26 +5,6 @@ var greetMe = function() {
   var dayOfWeek = document.getElementById('dayOfWeek').value;
   console.log('input value is = ', dayOfWeek);
 
-  switch (dayOfWeek) {
-    case 'Monday':
-      console.log(dayOfWeek + " is the worst day of the week");
-      break;
-    case 'Tuesday':
-      console.log("Taco " + dayOfWeek);
-      break;
-    case 'Wednesday':
-      console.log("Hump Day is "+ dayOfWeek);
-      break;
-    case 'Saturday':
-    // -- fall through --
-    case 'Sunday':
-      console.log('Happy Weekend!');
-      break;
-    default:
-      console.log("Happy " + dayOfWeek);
-  }
-};
-
 
 if('2' < 193804){
   console.log('inside if');
